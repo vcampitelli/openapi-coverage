9279b4efa0ca9094cace039b6ad42c28ee609396
Vinícius Campitelli <1877191+vcampitelli@users.noreply.github.com>
2024-12-17 10:15:36 -0300

    :bug: Adding check for duplicate routes to the PHP parser

--- parsers/php/src/Output.php
+++ parsers/php/src/Output.php
@@ -8,9 +8,15 @@ class Output
 {
     private const TYPE_ENDPOINT = 'endpoint';
 
+    private array $found = [];
+
     public function endpoint(string $method, string $uri, string $file = null, int $line = null): self
     {
-        echo self::TYPE_ENDPOINT . "\t{$method}\t{$uri}\t{$file}" . (($line === null) ? null : "\t{$line}") . "\n";
+        $key = \strtolower("{$method}_{$uri}");
+        if (!isset($this->found[$key])) {
+            $this->found[$key] = true;
+            echo self::TYPE_ENDPOINT . "\t{$method}\t{$uri}\t{$file}" . (($line === null) ? null : "\t{$line}") . "\n";
+        }
         return $this;
     }
 }
